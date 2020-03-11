72b39f8eb554d3df0ca00fed4d168e7a1779c932
surg <C:\Users\surg\AppData\Roaming\The Bat!>
2020-03-11 15:30:39 +0300

    Initial commit

---
+++ src/by/home/task/Pribor.java
@@ -0,0 +1,11 @@
+package by.home.task;
+
+public class Pribor {
+   private String namePrib; //Название прибора
+   private String modelPrib; //Модель прибора
+   private int powerPrib; //Мощность прибора
+   private int groupNom; // Номер группы прибора
+    public Pribor(String namePrib, String modelPrib,int powerPrib, int groupNom){
+        
+    }
+}
