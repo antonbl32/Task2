f112b3e3f37f97e7327be5d22f51e2919759e42e
surg <antonbl32>
2020-03-12 08:25:13 +0300

    change date

--- src/by/home/task/PriborKuhn.java
+++ src/by/home/task/PriborKuhn.java
@@ -1,22 +1,28 @@
 package by.home.task;
 
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
 import java.util.Date;
 
 public class PriborKuhn extends Pribor {
-    private String dataTO; //Дата следующего технического обслуживания
+    private final SimpleDateFormat sd=new SimpleDateFormat("dd.MM.yyyy");
+    private Date dataTO; //Дата следующего технического обслуживания
     private int emkost; //Масса или обьем загружаемых продуктов(посуды)
 
-    public PriborKuhn(String namePrib, String modelPrib, int powerPrib, int groupNom, String dataTO,int emkost) {
+    public PriborKuhn(String namePrib, String modelPrib, int powerPrib, int groupNom, String dataTO,int emkost) throws ParseException {
         super(namePrib, modelPrib, powerPrib, groupNom);
-        this.dataTO=dataTO;
-        this.emkost=emkost;
+        try {
+            this.dataTO = sd.parse(dataTO);
+            this.emkost = emkost;
+        } catch (ParseException s) {
+            System.out.println("Не верный формат данных");
+        }
     }
-
-    public String getDataTO() {
+    public Date getDataTO() {
         return dataTO;
     }
 
-    public void setDataTO(String dataTO) {
+    public void setDataTO(Date dataTO) {
         this.dataTO = dataTO;
     }
 
--- src/by/home/task/PriborVann.java
+++ src/by/home/task/PriborVann.java
@@ -1,27 +1,36 @@
 package by.home.task;
 
-public class PriborVann extends Pribor{
-   private String dataTO; //Дата следующего технического обслуживания
-   private String dataZamFiltr; //Дата замены фильтра или сменного элемента
-    public PriborVann(String namePrib, String modelPrib, int powerPrib, int groupNom, String dataTO,String zamFiltr) {
-        super(namePrib, modelPrib, powerPrib, groupNom);
-        this.dataTO=dataTO;
-        this.dataZamFiltr=zamFiltr;
-    }
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.util.Date;
 
-    public String getDataTO() {
+public class PriborVann extends Pribor{
+    private final SimpleDateFormat sd=new SimpleDateFormat("dd.MM.yyyy");
+    private Date dataTO; //Дата следующего технического обслуживания
+    private Date dataZamFiltr; //Дата замены фильтра или сменного элемента
+    public PriborVann(String namePrib, String modelPrib, int powerPrib, int groupNom, String dataTO,String zamFiltr) throws ParseException {
+        super(namePrib, modelPrib, powerPrib, groupNom);
+        try {
+            this.dataTO = sd.parse(dataTO);
+            this.dataZamFiltr = sd.parse(zamFiltr);
+
+        }catch (ParseException s){
+            System.out.println("Не верный формат данных");
+        }
+    }
+    public Date getDataTO() {
         return dataTO;
     }
 
-    public void setDataTO(String dataTO) {
+    public void setDataTO(Date dataTO) {
         this.dataTO = dataTO;
     }
 
-    public String getDataZamFiltr() {
+    public Date getDataZamFiltr() {
         return dataZamFiltr;
     }
 
-    public void setDataZamFiltr(String dataZamFiltr) {
+    public void setDataZamFiltr(Date dataZamFiltr) {
         this.dataZamFiltr = dataZamFiltr;
     }
 }
